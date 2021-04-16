aeaa54a263efe4393d2b86666a321edf1fa777ed
Subhankar Sen <SUBHANKARSEN2001@GMAIL.COM>
2021-04-17 01:48:54 +0530

    added the cnn classifiers

---
+++ CNN/train_JJd_motion.py
@@ -0,0 +1,202 @@
+import os
+from os.path import basename, join, exists
+os.chdir(r"/UTD-MHAD/Features/Image/JJd_motion")
+!ls
+folder=r"train/"
+total=0
+print('---Training set details----')
+for sub_folder in os.listdir(folder):
+  no_of_images=len(os.listdir("train/" + sub_folder))
+  total+=no_of_images
+  print(str(no_of_images) + " " + sub_folder + " images")
+
+print("Total no. of training images=",total)
+folder=r"val/"
+total=0
+print('---Test set details----')
+for sub_folder in os.listdir(folder):
+  no_of_images=len(os.listdir("val/" + sub_folder))
+  total+=no_of_images
+  print(str(no_of_images) + " " + sub_folder + " images")
+
+print("Total no. of validation images=",total)
+
+#importing necessary libraries and APIs
+import numpy as np
+import time
+import keras as keras
+from keras.layers import GlobalAveragePooling2D
+from keras.preprocessing import image
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Flatten
+from keras.layers import merge,Input
+from keras.models import Model
+from keras.utils import np_utils
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import load_model
+from numpy import array
+from numpy import argmax
+from sklearn.metrics import accuracy_score
+from  numpy import mean 
+from numpy import std
+import matplotlib.pyplot as plt
+from keras.optimizers import Adam,SGD
+from keras.callbacks import ReduceLROnPlateau,EarlyStopping,ModelCheckpoint
+from keras.layers import GlobalAveragePooling2D, Concatenate
+from keras.layers import BatchNormalization,Dropout
+from keras.layers import Lambda
+from keras.regularizers import l2
+import math
+from keras import backend as K
+from keras.metrics import categorical_accuracy
+import warnings
+warnings.filterwarnings('always')
+warnings.filterwarnings('ignore')
+from keras.models import load_model
+
+
+data_format = K.image_data_format()
+K.set_image_data_format(data_format)
+np.random.seed(42)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+num_of_classes = 27
+img_height =256
+img_width = 256
+batch_size =5
+input_shape = (img_height, img_width, 3)
+
+datagen= ImageDataGenerator()
+train_generator= datagen.flow_from_directory(
+    "train/",
+    target_size=(256,256),
+    color_mode='rgb',
+    batch_size=batch_size,
+    class_mode='categorical',
+    seed = 42
+   )
+
+val_generator= datagen.flow_from_directory(
+    "val/",
+    target_size=(256,256),
+    color_mode='rgb',
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle = False, 
+    seed = 42
+   )
+
+nb_train_samples = len(train_generator.filenames)
+nb_validation_samples = len(val_generator.filenames)
+predict_size_train = int(math.ceil(nb_train_samples / batch_size))
+predict_size_validation = int(math.ceil(nb_validation_samples / batch_size))
+
+np.random.seed(1000)
+
+#Instantiation
+AlexNet = Sequential()
+
+#1st Convolutional Layer
+AlexNet.add(Conv2D(filters=96, input_shape=(256,256,3), kernel_size=(11,11), strides=(4,4), padding='same'))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+AlexNet.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+
+#2nd Convolutional Layer
+AlexNet.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(1,1), padding='same'))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+AlexNet.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+
+#3rd Convolutional Layer
+AlexNet.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+
+#4th Convolutional Layer
+AlexNet.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same'))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+
+#5th Convolutional Layer
+AlexNet.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same'))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+AlexNet.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='same'))
+
+#Passing it to a Fully Connected layer
+AlexNet.add(Flatten())
+# 1st Fully Connected Layer
+AlexNet.add(Dense(4096, input_shape=(32,32,3,)))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+# Add Dropout to prevent overfitting
+AlexNet.add(Dropout(0.4))
+
+#2nd Fully Connected Layer
+AlexNet.add(Dense(4096))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+#Add Dropout
+AlexNet.add(Dropout(0.4))
+
+#3rd Fully Connected Layer
+AlexNet.add(Dense(1000))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('relu'))
+#Add Dropout
+AlexNet.add(Dropout(0.4))
+
+#Output Layer
+AlexNet.add(Dense(27))
+AlexNet.add(BatchNormalization())
+AlexNet.add(Activation('softmax'))
+
+#Model Summary
+AlexNet.summary()
+
+sgd = SGD(lr = 0.05, momentum = 0.9, clipnorm = 1.0)
+AlexNet.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+checkpoint1 = ModelCheckpoint('/UTD-MHAD/Features/Image/JJd_motion/JJd_motion_weights.h5', monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.7, min_delta = 0.0005,
+                              patience=20, min_lr=0.0001, verbose = 1)
+callbacks_list = [checkpoint1,reduce_lr]
+
+history =AlexNet.fit(
+    train_generator, 
+    epochs=500,
+    validation_data = val_generator,
+    callbacks=callbacks_list)
+
+# Accuracy vs Epoch curve
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train','Validation'], loc='upper left')
+plt.show()
+
+# Loss vs Epoch curve
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train','Validation'], loc='upper left')
+plt.show()
+
+AlexNet.save('/content/drive/MyDrive/UTD-MHAD/Features/Image/JJd_motion/JJd_motion_model.h5')
+loaded_model=load_model('/content/drive/MyDrive/UTD-MHAD/Features/Image/JJd_motion/JJd_motion_model.h5',compile=False)
+loaded_model.load_weights('/content/drive/MyDrive/UTD-MHAD/Features/Image/JJd_motion/JJd_motion_weights.h5')
+validation_labels=val_generator.classes
+validation_labels = keras.utils.to_categorical(validation_labels, num_classes=27)
+
+preds = loaded_model.predict(val_generator)
+predictions = [i.argmax() for i in preds]
+y_true = [i.argmax() for i in validation_labels ]
+print('Val Accuracy={}'.format(accuracy_score(y_true=y_true, y_pred=predictions)))
