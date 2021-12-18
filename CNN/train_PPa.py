610b6e70b0ee7105c86a5118f8134a059a30a937
Subhankar Sen <SUBHANKARSEN2001@GMAIL.COM>
2021-12-19 01:35:16 +0530

    PPa feat image encoding classifier

--- CNN/train_PPa.py
+++ CNN/train_PPa.py
@@ -20,7 +20,6 @@ for sub_folder in os.listdir(folder):
 
 print("Total no. of validation images=",total)
 
-#importing necessary libraries and APIs
 import numpy as np
 import time
 import keras as keras
